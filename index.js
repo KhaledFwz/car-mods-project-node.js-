37da84e49d0cffd0b9b35d717db6411ec362bde9
KhaledFwz <4everblackcat5000@gmail.com>
2025-05-03 01:28:18 +0300

    adding home page

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
-// app.use('/', (req,res) => {
-//     res.send("API is working")
-// })
+app.use('/', (req,res) => {
+    res.render("home")
+})
 
 // routes
 const CarBrandRoutes = require("./routes/CarBrandRoutes")
